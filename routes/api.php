caa74bd640cd0705eeaf8538d56be1d1eef0def5
carlomarespinosa <carlomar@wendyourway.com>
2022-02-23 06:19:47 +0800

    added auth register, login and forgot password

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [LoginController::class, 'login'])->name('api.login');
+Route::put('register', [RegisterController::class, 'register'])->name('api.register');
+
+Route::controller(ForgotPasswordController::class)->prefix('password')->group(function () {
+    Route::post('/forgot', 'forgot')->name('api.password.forgot');
+    Route::post('/reset', 'reset')->name('api.password.reset');
 });
